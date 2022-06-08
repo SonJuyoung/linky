905835ab33f9e4fc4313f67b0a4001020eb4e5ec
dbdjs <yueonsu@gmail.com>
2022-06-08 20:52:08 +0900

    eonsu

--- src/main/resources/static/js/apply/apply.js
+++ src/main/resources/static/js/apply/apply.js
@@ -1,5 +1,15 @@
 const applyContainerElem = document.querySelector('.apply-container');
 if(applyContainerElem) {
+    const selectedIndex = {
+        idx : ''
+    };
+
+    const data = {
+        q1 : '',
+        q2 : '',
+        q3 : '',
+        q4 : ''
+    };
 
     /* input태그 다음버튼 활성화 */
     const questionInput = applyContainerElem.querySelectorAll('.q-input');
@@ -23,12 +33,22 @@ if(applyContainerElem) {
         const button = parent.querySelector('.next');
         const bool = elem[elem.selectedIndex].value === '0' ? false : true;
         if(bool) {
+            data.q1 = elem[elem.selectedIndex].innerText;
+            selectedIndex.idx = elem.selectedIndex;
             button.classList.remove('hidden');
         } else {
             button.classList.add('hidden');
         }
-    }
 
+        if(selectedIndex.idx > 0) {
+            questionSelect.forEach(item => {
+                item[selectedIndex.idx].selected = true;
+                const parent = item.closest('.question');
+                const button = parent.querySelector('.next');
+                button.classList.remove('hidden');
+            });
+        }
+    }
     const questionSelect = applyContainerElem.querySelectorAll('.q-select-1');
     if(questionSelect.length > 2) {
         questionSelect.forEach(item => {
@@ -39,10 +59,16 @@ if(applyContainerElem) {
         });
     }
 
+    /* 다음,제출 버튼 */
     const nextArr = applyContainerElem.querySelectorAll('.next');
+    const maxSwiperPage = (applyContainerElem.querySelectorAll('.swiper-slide').length / 3) - 1;
     nextArr.forEach(item => {
         item.addEventListener('click', () => {
-            slide.slideNext(1, 10, 1);
+            if(slide.realIndex < maxSwiperPage) {
+                slide.slideNext(1, 10, 1);
+            } else {
+                /** submit 기능 **/
+            }
         });
     });
 }
