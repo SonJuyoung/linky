ee2f7f243a0d6702d4f2e00c8d6d9f8ab5d409fb
dbdjs <yueonsu@gmail.com>
2022-06-08 22:32:01 +0900

    eonsu

--- src/main/java/com/example/linky/apply/ApplyController.java
+++ src/main/java/com/example/linky/apply/ApplyController.java
@@ -1,7 +1,11 @@
 package com.example.linky.apply;
 
+import com.example.linky.apply.model.AnswerListVo;
+import com.example.linky.apply.model.AnswerVo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +16,13 @@ public class ApplyController {
     public String apply() {
 
         return "apply/apply";
+    }
+
+    @PostMapping
+    public String applyProc(AnswerListVo listVo) {
+        for(AnswerVo item : listVo.getAnswerVoList()) {
+            System.out.println(item.getAnswer());
+        }
+        return "redirect:/";
     }
 }
---
+++ src/main/java/com/example/linky/apply/model/AnswerListVo.java
@@ -0,0 +1,10 @@
+package com.example.linky.apply.model;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class AnswerListVo {
+    private List<AnswerVo> answerVoList;
+}
