def331dfe84e277e3044abc5fc2394ba8b2f94cf
juyoung <terranzz01@gmail.com>
2022-06-09 23:27:10 +0900

    카톡상담api 작업 중

--- src/main/resources/static/js/main/main.js
+++ src/main/resources/static/js/main/main.js
@@ -48,3 +48,22 @@ scheduleCancelButton.addEventListener('click', () => {
     modSwitchFunction(scheduleDataArr, 'off');
     changeScheduleModButton();
 });
+
+// Process Swiper
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
